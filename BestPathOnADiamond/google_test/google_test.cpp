1c68fb4221b6f3e83e27b3ba5a26d112e17e7128
INFRAWARE\kos0120 <zzzharuki2@gmail.com>
2016-12-21 11:32:39 +0900

    modify memorize logic

--- BestPathOnADiamond/google_test/google_test.cpp
+++ BestPathOnADiamond/google_test/google_test.cpp
@@ -1,3 +1,4 @@
+/*
 #include "stdafx.h"
 #include <gtest/gtest.h>
 #include <array>
@@ -36,7 +37,7 @@ public:
 	value_type find_path(const input_type& input)
 	{
 		reset(input);
-		value_type result = find(input, 0, 0, 0);
+		value_type result = find(input, 0, 0);
 		return result;
 	}
 private:
@@ -50,26 +51,24 @@ private:
 			for (auto& c : r)
 				c = unmemorized;
 	}
-	const value_type find(const input_type& input, size_type r, size_type c, value_type sum)
+	const value_type find(const input_type& input, size_type r, size_type c)
 	{
-		if( input.size() > r  && c >= 0 && input[r].size() > c)
-		{
-			if(memo[r][c] == unmemorized)
-				memo[r][c] = sum + input[r][c];
-			else
-				memo[r][c] = std::max(memo[r][c], sum + input[r][c]);
-		}
-		else
-			return sum;
+		if(c < 0 || r >= input.size() || c >= input[r].size() )
+			return 0;
 
-		if(r < max_col)
-			// down or right
-			sum = std::max(find(input, r+1, c, memo[r][c]), find(input, r+1, c+1, memo[r][c]));
-		else
-			// down or left
-			sum = std::max(find(input, r+1, c, memo[r][c]), find(input, r+1, c-1, memo[r][c]));
+		value_type input_value = input[r][c];
+		if(r == input.size() - 1)
+			return input_value;
 
-		return sum;
+		value_type& memo_value = memo[r][c];
+		if(memo_value != unmemorized)
+			return memo_value;
+
+		value_type down = find(input, r+1, c);
+		value_type down_right = r < max_col ? find(input, r+1, c+1) : 0;
+		value_type down_left = r >= max_col ? find(input, r+1, c-1) : 0;
+		memo_value = std::max( std::max(down, down_right), down_left ) + input_value;
+		return memo_value;
 	}
 	input_type memo;
 	size_type max_col;
@@ -88,59 +87,96 @@ template <typename Tratis, typename Array>
 diamond::input_type array_to_vector(const Array& array)
 {
 	diamond::input_type input;
-	
+// 	auto& it = array.begin();
+// 	bool dir = true; // true : increase, false : decrease
+// 	size_t col_max = 1;
+// 	for (size_t r = 0; r < Tratis::row; ++r)
+// 	{
+// 		diamond::col_type col;
+//  		for (size_t c = 0; c < col_max; ++c)
+//  		{
+// 			col.push_back(*it);
+// 			it++;
+//  		}
+// 		input.push_back(col);
+// 
+// 		if(dir)
+// 			col_max++;
+// 		else
+// 			col_max--;
+// 		if(col_max == Tratis::col)
+// 			dir = false;
+// 	}
 	auto& it = array.begin();
-	bool dir = true; // true : increase, false : decrease
 	size_t col_max = 1;
 	for (size_t r = 0; r < Tratis::row; ++r)
 	{
 		diamond::col_type col;
- 		for (size_t c = 0; c < col_max; ++c)
- 		{
+		for (size_t c = 0; c < Tratis::col; ++c)
+		{
 			col.push_back(*it);
 			it++;
- 		}
+		}
 		input.push_back(col);
-
-		if(dir)
-			col_max++;
-		else
-			col_max--;
-		if(col_max == Tratis::col)
-			dir = false;
 	}
 	return input;
 }
 
 TEST_F(diamond_test, simple_test)
 {
-	std::array<diamond::value_type, array_traits<5>::size> input1 =
+	std::array<diamond::value_type, array_traits<5>::capacity> input1 =
 	{
-		6,/*0,0,0,0,*/
-		1,2,/*0,0,0,*/
-		6,7,4,/*0,0,*/
-		9,4,1,7,/*0,*/
+		6,0,0,0,0,
+		1,2,0,0,0,
+		6,7,4,0,0,
+		9,4,1,7,0,
 		6,7,5,9,4,
-		4,4,3,2,/*0,*/
-		1,2,3,/*0,0,*/
-		6,1,/*0,0,0,*/
-		7/*,0,0,0,0*/
+		4,4,3,2,0,
+		1,2,3,0,0,
+		6,1,0,0,0,
+		7,0,0,0,0
 	};
 
-	std::array<diamond::value_type, array_traits<5>::size> input2 =
+	std::array<diamond::value_type, array_traits<5>::capacity> input2 =
 	{
-		39,/*0,0,0,0,*/
-		43,16,/*0,0,0,*/
-		74,94,24,/*0,0,*/
-		25,76,98,62,/*0,*/
+		39,0,0,0,0,
+		43,16,0,0,0,
+		74,94,24,0,0,
+		25,76,98,62,0,
 		79,58,71,67,98,
-		43,55,27,44,/*0,*/
-		10,96,56,/*0,0,*/
-		73,31,/*0,0,0,*/
-		95/*,0,0,0,0*/
+		43,55,27,44,0,
+		10,96,56,0,0,
+		73,31,0,0,0,
+		95,0,0,0,0
+	};
+
+	std::array<diamond::value_type, array_traits<2>::capacity> input3 =
+	{
+		1,0,
+		2,3,
+		4,0
+	};
+
+	std::array<diamond::value_type, array_traits<3>::capacity> input4 =
+	{
+		1,0,0,
+		2,3,0,
+		4,5,6,
+		7,8,0,
+		9,0,0
+	};
+
+	std::array<diamond::value_type, array_traits<2>::capacity> input5 =
+	{
+		1,0,
+		3,2,
+		4,0
 	};
 
 	diamond tester;
+	EXPECT_EQ(8, tester.find_path(std::move(array_to_vector< array_traits<2> >(input5) )));
+	EXPECT_EQ(27, tester.find_path(std::move(array_to_vector< array_traits<3> >(input4) )));
+	EXPECT_EQ(8, tester.find_path(std::move(array_to_vector< array_traits<2> >(input3) )));
 	EXPECT_EQ(48, tester.find_path(std::move(array_to_vector< array_traits<5> >(input1) )));
 	EXPECT_EQ(664, tester.find_path(std::move(array_to_vector< array_traits<5> >(input2) )));
 
@@ -151,3 +187,91 @@ int main(int argc, char* argv[])
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 } 
+*/
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+
+class diamond
+{
+public:
+	typedef int size_type;
+	typedef int value_type;
+	typedef std::vector<value_type> col_type;
+	typedef std::vector<col_type> row_type;
+	typedef row_type input_type;
+	diamond(){}
+	value_type find_path(input_type&& input)
+	{
+		return find_path(input);
+	}
+
+	value_type find_path(const input_type& input)
+	{
+		reset(input);
+		value_type result = find(input, 0, 0);
+		return result;
+	}
+private:
+	static const value_type unmemorized = -1;
+	void reset(const input_type& input)
+	{
+		max_col = input.size() / 2 ;
+		memo.resize(0);
+		memo = input;
+		for (auto& r : memo)
+			for (auto& c : r)
+				c = unmemorized;
+	}
+	const value_type find(const input_type& input, size_type r, size_type c)
+	{
+		if(c < 0 || r >= input.size() || c >= input[r].size() )
+			return 0;
+
+		value_type input_value = input[r][c];
+		if(r == input.size() - 1)
+			return input_value;
+
+		value_type& memo_value = memo[r][c];
+		if(memo_value != unmemorized)
+			return memo_value;
+
+		value_type down = find(input, r+1, c);
+		value_type down_right = r < max_col ? find(input, r+1, c+1) : 0;
+		value_type down_left = r >= max_col ? find(input, r+1, c-1) : 0;
+		memo_value = std::max( std::max(down, down_right), down_left ) + input_value;
+		return memo_value;
+	}
+	input_type memo;
+	size_type max_col;
+};
+
+int main(int argc, char* argv[])
+{
+	using namespace std;
+	size_t case_count, col_size;
+	cin >> case_count;
+	for(int id = 0; id < case_count; id++)
+	{
+		cin >> col_size;
+		diamond tester;
+		diamond::input_type input;
+		size_t row_size = 2 * col_size - 1;
+		for (size_t i = 0; i < row_size; ++i)
+		{
+			diamond::col_type col;
+			col.resize(col_size);
+			input.push_back(col);
+		}
+
+		for(size_t y = 0; y < col_size; y++)
+			for(size_t x = 0; x <= y; x++)
+				cin >> input[y][x];
+		for(size_t y = col_size; y< row_size; y++)
+			for(size_t x = 0; x < 2 * col_size - 1 - y; x++)
+				cin >> input[y][x];
+		cout<<tester.find_path(input)<<endl;
+	}
+	return 0;
+}
